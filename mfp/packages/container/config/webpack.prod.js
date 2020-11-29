c7c7cbffe3c7dd0ef307b29ff9c90957697e8d52
Mustafa SEYMEN <sevgi.gmail.com>
2020-11-29 22:47:21 +0300

    .

--- mfp/packages/container/config/webpack.prod.js
+++ mfp/packages/container/config/webpack.prod.js
@@ -4,12 +4,14 @@ const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 //const domain = process.env.PRODUCTION_DOMAIN;  // production'ın nerede olacağını içerecek.
 const domain = 'http://localhost:5556';
+const path = require('path');
 
 const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js', // adlandırma şablonu
-        publicPath: '/container/latest/' // main file'a erişilecek path '/container/latest/main.js'
+        publicPath: '/container/latest/', // main file'a erişilecek path '/container/latest/main.js'
+        path: path.resolve(__dirname,'../marketing/v1/')
     },
     plugins: [
         new ModuleFederationPlugin({
